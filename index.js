f1534154e1a77ce12a38ac2381fa42f24a15152a
Henrique Souza NGI <henrique.souza@ngi.com.br>
2021-10-20 18:42:32 -0300

    chore: users insertion

--- index.js
+++ index.js
@@ -1,12 +1,51 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost/mongodbtest', {
-    useMongoClient: true
-})
+mongoose.connect('mongodb://localhost/mongodbtest')
     .then(() => {
         console.log('Successful connection')
     })
     .catch((err) => {
         console.log('Error at connectiong db; ' + err)
+    })
+
+//Model - Users
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    subname: {
+        type: String,
+        require: true
+    },
+    mail: {
+        type: String,
+        require: true
+    },
+    age: {
+        type: Number,
+        require: true
+    },
+    country: {
+        type: String
+    }
+})
+
+mongoose.model('users', UserSchema)
+
+const newUser = mongoose.model('users')
+
+new newUser({
+    name: 'Fulano',
+    subname: 'Silva',
+    mail: 'fulano@mail.com.br',
+    age: 25,
+    country: 'Brazil'
+}).save()
+    .then(() => {
+        console.log('User created')
+    })
+    .catch((err) => {
+        console.log('An error apears: ' + err)
     })
